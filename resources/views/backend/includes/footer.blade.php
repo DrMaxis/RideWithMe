7f9a10da6ebeeeecda875b5b0f6d85de9f9906b5
DrMaxis <Grandmasterantwii@gmail.com>
2019-05-17 15:53:03 -0400

    Init

---
+++ resources/views/backend/includes/footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="app-footer">
+    <div>
+        <strong>@lang('labels.general.copyright') &copy; {{ date('Y') }}
+            <a href="#">
+                @lang('strings.backend.general.website_link')
+            </a>
+        </strong> @lang('strings.backend.general.all_rights_reserved')
+    </div>
+
+    <div class="ml-auto">Theme by <a href="http://coreui.io">CoreUI</a></div>
+</footer>
