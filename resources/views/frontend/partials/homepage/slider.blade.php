381e72db6f2631ac2e56de89755d0428b9ff2078
DrMaxis <Grandmasterantwii@gmail.com>
2019-06-13 01:07:43 -0400

    Update slider.blade.php

--- resources/views/frontend/partials/homepage/slider.blade.php
+++ resources/views/frontend/partials/homepage/slider.blade.php
@@ -9,7 +9,7 @@
 
                   
 
-                    <div class="tp-caption sfb fadeout slider-caption slider-caption-1" data-x="center" data-y="280" data-speed="700" data-easing="easeOutBack"  data-start="2000">The Best Carpooling Hub In Ghana</div>
+                    <div class="tp-caption sfb fadeout slider-caption slider-caption-1" data-x="center" data-y="280" data-speed="700" data-easing="easeOutBack"  data-start="2000">The Best Carpooling Hub In Africa</div>
                     
                     <a href="#" class="tp-caption sfb fadeout awe-btn awe-btn-style3 awe-btn-slider" data-x="center" data-y="380" data-easing="easeOutBack" data-speed="700" data-start="2200">Find A Ride</a>
                 </li> 
